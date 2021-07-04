14325e77ef7f484ffcf54c5db99423d009e5645c
Myo Aung Hlaing <mahlaing@gmail.com>
2021-07-04 15:21:43 +1000

    modify to handle empty repository

--- AusiExGitExplorer/Program.cs
+++ AusiExGitExplorer/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
@@ -35,50 +36,68 @@ namespace AusiExGitExplorer
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", token);
                     client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "http://developer.github.com/v3/#user-agent-required");
 
-                    var respRepos = await client.GetAsync($"{baseUrl}/user/repos");
-                    log.Info($"Response status on getting user repos: {respRepos.StatusCode}");
-                    respRepos.EnsureSuccessStatusCode();
+                    var respRepos = await client.GetAsync($"{baseUrl}/user/repos");                    
 
-                    JArray contents = (JArray)JsonConvert.DeserializeObject(respRepos.Content.ReadAsStringAsync().Result);
                     List<string> repoList = new List<string>();
-
-                    log.Info("\n\nRepositories user has access");
-                    log.Info("=================================================");
-
-                    foreach (JObject content in contents.Children<JObject>())
+                    if (respRepos.IsSuccessStatusCode)
                     {
-                        string repo = (string)content["name"];
-                        repoList.Add(repo);
-                        log.Info("id: " + (string)content["id"]);
-                        log.Info("name: " + repo);
-                        log.Info("full_name: " + (string)content["full_name"]);
-                        log.Info("html_url: " + (string)content["html_url"]);
-                        log.Info("description: " + (string)content["description"]);
+                        JArray contents = (JArray)JsonConvert.DeserializeObject(respRepos.Content.ReadAsStringAsync().Result);                       
+
+                        log.Info("\n\nRepositories user has access");
                         log.Info("=================================================");
+
+                        foreach (JObject content in contents.Children<JObject>())
+                        {
+                            string repo = (string)content["name"];
+                            repoList.Add(repo);
+                            log.Info("id: " + (string)content["id"]);
+                            log.Info("name: " + repo);
+                            log.Info("full_name: " + (string)content["full_name"]);
+                            log.Info("html_url: " + (string)content["html_url"]);
+                            log.Info("description: " + (string)content["description"]);
+                            log.Info("=================================================");
+                        }
+                    }
+                    else
+                    {
+                        log.Error("Error in getting repositories info");
+                        log.Error($"Response status: {respRepos.StatusCode}");
                     }
 
                     log.Info("\n\nLatest 10 commits of the repositories");
                     log.Info("=================================================");
+
                     foreach (string repo in repoList)
                     {
                         string url = $"{baseUrl}/repos/{user}/{repo}/commits?per_page=10";
 
-                        var respCommits = await client.GetAsync(url);
-                        log.Info($"Response status on getting repo {repo} commits: {respRepos.StatusCode}");
-                        respCommits.EnsureSuccessStatusCode();
-
-                        JArray commits = (JArray)JsonConvert.DeserializeObject(respCommits.Content.ReadAsStringAsync().Result);
-                        log.Info("\n\nrepository: " + repo);
-                        log.Info("=================================================");
-                        foreach (JObject com in commits.Children<JObject>())
+                        var respCommits = await client.GetAsync(url);                        
+                        
+                        if (respCommits.IsSuccessStatusCode)
                         {
-                            log.Info("sha: " + (string)com["sha"]);
-                            log.Info("author name: " + (string)com["commit"]["author"]["name"]);
-                            log.Info("email: " + (string)com["commit"]["author"]["email"]);
-                            log.Info("date: " + (string)com["commit"]["author"]["date"]);
-                            log.Info("message: " + (string)com["commit"]["message"]);
-                            log.Info("url: " + (string)com["url"]);
+                            JArray commits = (JArray)JsonConvert.DeserializeObject(respCommits.Content.ReadAsStringAsync().Result);
+                            log.Info("\n\nrepository: " + repo);
                             log.Info("=================================================");
+                            foreach (JObject com in commits.Children<JObject>())
+                            {
+                                log.Info("sha: " + (string)com["sha"]);
+                                log.Info("author name: " + (string)com["commit"]["author"]["name"]);
+                                log.Info("email: " + (string)com["commit"]["author"]["email"]);
+                                log.Info("date: " + (string)com["commit"]["author"]["date"]);
+                                log.Info("message: " + (string)com["commit"]["message"]);
+                                log.Info("url: " + (string)com["url"]);
+                                log.Info("=================================================");
+                            }
+                        }
+                        else if(respCommits.StatusCode == HttpStatusCode.Conflict)
+                        {
+                            log.Info($"\n\nNo commit found for the repository {repo}");
+                            log.Info("=================================================");
+                        }
+                        else
+                        {
+                            log.Error("Error in getting commits info");
+                            log.Error($"Response status: {respRepos.StatusCode}");
                         }
                     }           
                     Console.ReadKey();
